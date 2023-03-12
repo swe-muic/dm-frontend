266f4ce2f6287696e45d4751d4f312a109320f53
Rinradee Sae-lao <rinradee.sae@gmail.com>
2023-03-12 07:47:21 +0700

    fixed signout redirection

--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -2,15 +2,24 @@
 
 import React from 'react';
 import { getAuth, signOut } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 export interface IGraphsProps {}
 
 const Graphs: React.FunctionComponent<IGraphsProps> = () => {
 	const auth = getAuth();
+	const navigate = useNavigate();
 	return (
 		<div>
 			<h1>This is the My Graphs section, where your saved graphs are stored</h1>
-			<button onClick={() => signOut(auth)}>Sign Out</button>
+			<button
+				onClick={() => {
+					signOut(auth);
+					navigate('/');
+				}}
+			>
+				Sign Out
+			</button>
 		</div>
 	);
 };
