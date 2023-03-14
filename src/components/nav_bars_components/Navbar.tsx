708728617a226dadde7ab876cd2e5d3074ac826d
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-03-15 02:24:59 +0700

    create condition if user did not save the graph the delete icon will not appear

--- src/components/nav_bars_components/Navbar.tsx
+++ src/components/nav_bars_components/Navbar.tsx
@@ -3,20 +3,29 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import TextField from '@mui/material/TextField';
-import UserIcon from './userIconButton';
-import DeleteIcon from './deleteIconButton';
-import SaveIcon from './saveIconButton';
+import UserIcon from './nav_bar_button/userIconButton';
+import DeleteIcon from './nav_bar_button/deleteIconButton';
+// import SaveIcon from './nav_bar_button/saveIconButton';
 import NavBarTitle from './navBarTitle';
-import EditBarIcon from './editIconButton';
-import MenuIcon from './menuIconButton';
+import EditBarIcon from './nav_bar_button/editIconButton';
+import MenuIcon from './nav_bar_button/menuIconButton';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import IconButton from '@mui/material/IconButton';
 
 export default function Navbar(): React.ReactElement {
 	const [isLogIn, setIsLogin] = useState(false);
+	const [isSave, setIsSave] = useState(false);
 	const graphTitle = 'GRAPH TITLE';
-	const handleClick = (): void => {
+	const handleLoginRegisClick = (): void => {
 		setIsLogin(!isLogIn);
+	};
+
+	const handleSaveIconClick = (): void => {
+		if (!isSave) {
+			setIsSave(!isSave);
+		}
 	};
 
 	return (
@@ -44,17 +53,26 @@ export default function Navbar(): React.ReactElement {
 
 					{isLogIn ? (
 						<Stack direction='row'>
-							<SaveIcon />
-							<DeleteIcon />
+							<IconButton
+								size='large'
+								edge='start'
+								color='inherit'
+								aria-label='menu'
+								sx={{ mr: 2 }}
+								onClick={handleSaveIconClick}
+							>
+								<SaveOutlinedIcon />
+							</IconButton>
+							{isSave ? <DeleteIcon /> : null}
 							<UserIcon />
 						</Stack>
 					) : (
 						<Stack spacing={4} direction='row'>
-							<Button color='inherit' onClick={handleClick}>
+							<Button color='inherit' onClick={handleLoginRegisClick}>
 								REGISTER
 							</Button>
 
-							<Button color='inherit' onClick={handleClick}>
+							<Button color='inherit' onClick={handleLoginRegisClick}>
 								LOGIN
 							</Button>
 						</Stack>
@@ -64,5 +82,3 @@ export default function Navbar(): React.ReactElement {
 		</Box>
 	);
 }
-
-// export default Navbar
