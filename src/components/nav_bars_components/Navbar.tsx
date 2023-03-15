2f58fe699da50c6d06fd3d78ace71b85beb6f3ae
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-03-15 22:44:58 +0700

    create double click to edit and make save edit to work

--- src/components/nav_bars_components/Navbar.tsx
+++ src/components/nav_bars_components/Navbar.tsx
@@ -5,19 +5,19 @@ import Toolbar from '@mui/material/Toolbar';
 import TextField from '@mui/material/TextField';
 import UserIcon from './nav_bar_button/userIconButton';
 import DeleteIcon from './nav_bar_button/deleteIconButton';
-// import SaveIcon from './nav_bar_button/saveIconButton';
 import NavBarTitle from './navBarTitle';
-import EditBarIcon from './nav_bar_button/editIconButton';
 import MenuIcon from './nav_bar_button/menuIconButton';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import IconButton from '@mui/material/IconButton';
+import BorderColorOutlinedIcon from '@mui/icons-material/BorderColorOutlined';
 
 export default function Navbar(): React.ReactElement {
 	const [isLogIn, setIsLogin] = useState(false);
+	const [isEdit, setIsEdit] = useState(false);
 	const [isSave, setIsSave] = useState(false);
-	const graphTitle = 'GRAPH TITLE';
+	const [buttonText, setButtonText] = useState('GRAPH TITLE');
 	const handleLoginRegisClick = (): void => {
 		setIsLogin(!isLogIn);
 	};
@@ -25,7 +25,18 @@ export default function Navbar(): React.ReactElement {
 	const handleSaveIconClick = (): void => {
 		if (!isSave) {
 			setIsSave(!isSave);
+			setButtonText(buttonText);
 		}
+	};
+
+	const handleEditGraphName = (): void => {
+		if (isLogIn) {
+			setIsEdit(!isEdit);
+		}
+	};
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		setButtonText(event.target.value);
 	};
 
 	return (
@@ -38,17 +49,35 @@ export default function Navbar(): React.ReactElement {
 			>
 				<Toolbar>
 					<MenuIcon />
-					<TextField
-						id='standard-basic'
-						variant='standard'
-						defaultValue={graphTitle}
-						sx={{
-							input: { color: 'white' },
-							label: { color: 'white' },
-						}}
-					/>
+					{!isEdit ? (
+						<Button color='inherit' onDoubleClick={handleEditGraphName}>
+							{buttonText}
+						</Button>
+					) : (
+						<TextField
+							id='standard-basic'
+							variant='standard'
+							defaultValue={buttonText}
+							onChange={handleChange}
+							sx={{
+								input: { color: 'white' },
+								label: { color: 'white' },
+							}}
+						/>
+					)}
+					{isLogIn ? (
+						<IconButton
+							size='large'
+							edge='start'
+							color='inherit'
+							aria-label='menu'
+							sx={{ mr: 2 }}
+							onClick={handleEditGraphName}
+						>
+							<BorderColorOutlinedIcon />
+						</IconButton>
+					) : null}
 
-					<EditBarIcon />
 					<NavBarTitle />
 
 					{isLogIn ? (
