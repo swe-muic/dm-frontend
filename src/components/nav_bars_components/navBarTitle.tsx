6cf3fd8ffea56396b45a34999096aa1ae7cf2714
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-03-16 01:23:22 +0700

    fixed the position of the title and the button

--- src/components/nav_bars_components/navBarTitle.tsx
+++ src/components/nav_bars_components/navBarTitle.tsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 
 export default function navBarTitle(): React.ReactElement {
 	return (
-		<Typography variant='h6' component='div' align={'center'} sx={{ flexGrow: 1 }}>
+		<Typography variant='h6' component='div' sx={{ flexGrow: 1 }} style={{ position: 'absolute', right: '50%' }}>
 			Deezmos
 		</Typography>
 	);
