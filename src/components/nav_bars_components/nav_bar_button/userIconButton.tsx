50ff0c17b0a314423d5adafcc3c6177052e466db
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-03-18 02:43:06 +0700

    do unit test for 80% coverage

--- src/components/nav_bars_components/nav_bar_button/userIconButton.tsx
+++ src/components/nav_bars_components/nav_bar_button/userIconButton.tsx
@@ -3,7 +3,14 @@ import IconButton from '@mui/material/IconButton';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 export default function userIconButton(): React.ReactElement {
 	return (
-		<IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+		<IconButton
+			data-testid='user-icon-button'
+			size='large'
+			edge='start'
+			color='inherit'
+			aria-label='menu'
+			sx={{ mr: 2 }}
+		>
 			<AccountCircle />
 		</IconButton>
 	);
