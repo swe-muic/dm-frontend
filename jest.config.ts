433abb91913774988e5f0f8dc14c9bca8834f99a
Rinradee Sae-lao <rinradee.sae@gmail.com>
2023-03-18 15:05:44 +0700

    login styling

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@ export default {
 		'^.+\\.tsx?$': 'babel-jest',
 		'^.+\\.svg$': 'jest-transform-stub',
 		'^.+\\.scss$': '<rootDir>/sassTransform.cjs',
-		'^.+\\.css$': 'jest-transform-stub',
+		'^.+\\.(css|scss|png|jpg|svg)$': 'jest-transform-stub',
 	},
 	testMatch: ['**/*.test.tsx'],
 	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import GoogleButton from 'react-google-button';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import login_background from '../images/login_background.png';
 
 const Login: React.FunctionComponent = () => {
 	const auth = getAuth();
@@ -19,10 +20,21 @@ const Login: React.FunctionComponent = () => {
 				setAuthentication(false);
 			});
 	};
+	const loginStyle = {
+		background: `url(${login_background})`,
+		height: '100vh',
+		marginTop: '10px',
+		color: 'white',
+		backgroundSize: 'cover',
+		backgroundRepeat: 'no-repeat',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	};
 	return (
-		<div>
-			<h1>This is the login page</h1>
+		<div style={loginStyle}>
 			<div>
+				<h1>Login</h1>
 				<GoogleButton
 					onClick={() => {
 						void signInWithGoogle();
