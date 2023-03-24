865920f9d946874078add853ec80bb15acc68479
suhavni <suhavni@gmail.com>
2023-03-24 16:49:57 +0700

    update minio config for deprecated keys

--- src/services/minio/minioClient.ts
+++ src/services/minio/minioClient.ts
@@ -3,8 +3,10 @@ import AWS from 'aws-sdk';
 const minioClient = new AWS.S3({
 	endpoint: 'http://localhost:9000',
 	s3ForcePathStyle: true,
-	accessKeyId: process.env.REACT_APP_MINIO_ROOT_USER,
-	secretAccessKey: process.env.REACT_APP_MINIO_ROOT_PASSWORD,
+	credentials: {
+		accessKeyId: process.env.REACT_APP_MINIO_ROOT_USER ?? '',
+		secretAccessKey: process.env.REACT_APP_MINIO_ROOT_PASSWORD ?? '',
+	},
 });
 
 export default minioClient;
