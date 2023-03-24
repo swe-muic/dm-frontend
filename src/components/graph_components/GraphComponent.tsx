486386382f1c45e84f0e370c464317f178b50c46
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-03-24 15:54:15 +0700

    comment graph

--- src/components/graph_components/GraphComponent.tsx
+++ src/components/graph_components/GraphComponent.tsx
@@ -13,24 +13,40 @@ const Plot = ({ data, options }: PlotProps) => {
 	const containerRef = useRef<HTMLDivElement>(null);
 
 	useEffect(() => {
-		if (containerRef.current) {
-			functionPlot({
-				...options,
-				tip: {
-					xLine: false,
-					yLine: false,
-				},
-				target: containerRef.current,
-				grid: true,
-				width: frames.innerWidth,
-				height: frames.innerHeight * 0.9,
+		try {
+			if (containerRef.current) {
+				functionPlot({
+					...options,
+					tip: {
+						xLine: true,
+						yLine: true,
+					},
+					target: containerRef.current,
+					grid: true,
+					width: window.innerWidth,
+					height: window.innerHeight,
 
-				data,
-			});
+					data,
+				});
+			}
+		} catch (e) {
+			if (containerRef.current) {
+				functionPlot({
+					...options,
+					tip: {
+						xLine: true,
+						yLine: true,
+					},
+					target: containerRef.current,
+					grid: true,
+					width: window.innerWidth,
+					height: window.innerHeight,
+				});
+			}
 		}
 	}, [data, options]);
 
-	return <div ref={containerRef} style={{ width: '100%', height: '100%', marginTop: 50 }} />;
+	return <div ref={containerRef} style={{ width: '100%', height: '100%', marginTop: 100 }}></div>;
 };
 
 export default Plot;
