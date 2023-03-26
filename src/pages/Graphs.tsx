915cfa81211278853c97763ed6e6ba80317d6d2f
Rinradee Sae-lao <rinradee.sae@gmail.com>
2023-03-26 14:12:02 +0700

    fixed firebase key

--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -1,23 +1,30 @@
 import React from 'react';
-import { getAuth, signOut } from 'firebase/auth';
+import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../config/FirebaseConfig';
+import Button from '@mui/material/Button';
+import AuthRoute from '../components/AuthRoute';
 
 const Graphs: React.FunctionComponent = () => {
-	const auth = getAuth();
+	const thisAuth = auth;
 	const navigate = useNavigate();
+
 	return (
-		<div>
-			<h1>This is the My Graphs section, where your saved graphs are stored</h1>
-			<button
-				onClick={() => {
-					void signOut(auth).then(() => {
-						navigate('/');
-					});
-				}}
-			>
-				Sign Out
-			</button>
-		</div>
+		<AuthRoute>
+			<div>
+				<h1>This is the My Graphs section, where your saved graphs are stored</h1>
+				<Button
+					data-testid={'sign-out'}
+					onClick={() => {
+						void signOut(thisAuth).then(() => {
+							navigate('/');
+						});
+					}}
+				>
+					Sign Out
+				</Button>
+			</div>
+		</AuthRoute>
 	);
 };
 
