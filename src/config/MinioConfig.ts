45968f899063baae3796381d29061f04b3a6830d
suhavni <suhavni@gmail.com>
2023-04-03 00:12:56 +0700

    unskipped tests & used minio serv for testing

--- src/config/MinioConfig.ts
+++ src/config/MinioConfig.ts
@@ -2,7 +2,7 @@
 export const MinioConfig = {
 	endpoint: process.env.REACT_APP_MINIO_ENDPOINT ?? 'http://localhost:9000',
 	forcePathStyle: true,
-	region: 'us-east-1',
+	region: 'ap-east=1',
 	credentials: {
 		accessKeyId: process.env.REACT_APP_MINIO_ROOT_USER ?? '',
 		secretAccessKey: process.env.REACT_APP_MINIO_ROOT_PASSWORD ?? '',
