cae8e65c44e9b8088656bab80cb4924ef1f5b699
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-04-09 16:15:07 +0700

    add side drawer

--- src/components/NavBar/NavBarButton/menuIconButton.tsx
+++ src/components/NavBar/NavBarButton/menuIconButton.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Drawer } from '@mui/material';
 
 /* istanbul ignore next */
 
 export default function menuIconButton(): React.ReactElement {
+	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
 	return (
-		<IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
-			<MenuIcon />
-		</IconButton>
+		<div>
+			<IconButton
+				size='large'
+				edge='start'
+				color='inherit'
+				aria-label='menu'
+				sx={{ mr: 2 }}
+				onClick={() => {
+					setIsDrawerOpen(true);
+				}}
+			>
+				<MenuIcon />
+			</IconButton>
+			<Drawer
+				anchor='left'
+				open={isDrawerOpen}
+				onClose={() => {
+					setIsDrawerOpen(false);
+				}}
+			>
+				<Box p={2} width='500px' textAlign='center' role='presentation'>
+					<Typography variant='h6' component='div'>
+						Input Equation
+					</Typography>
+				</Box>
+			</Drawer>
+		</div>
 	);
 }
