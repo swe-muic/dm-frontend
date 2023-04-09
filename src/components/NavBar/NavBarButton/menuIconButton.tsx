ee6935d52180dc9140bf654d4fbb5ce9cdaa6016
Worawit Penglam <worawit.pen@student.mahidol.edu>
2023-04-09 18:45:42 +0700

    set up for the drawer

--- src/components/NavBar/NavBarButton/menuIconButton.tsx
+++ src/components/NavBar/NavBarButton/menuIconButton.tsx
@@ -6,10 +6,24 @@ import Typography from '@mui/material/Typography';
 import { Drawer } from '@mui/material';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import Stack from '@mui/material/Stack';
-/* istanbul ignore next */
+import TextField from '@mui/material/TextField';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function menuIconButton(): React.ReactElement {
 	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+	const [numFields, setNumFields] = useState(1);
+	const [equations, setEquations] = useState(['']);
+
+	const addField = (): void => {
+		setNumFields(numFields + 1);
+		setEquations([...equations, '']);
+	};
+
+	const handleChange = (index: number, value: string): void => {
+		const newEquations = [...equations];
+		newEquations[index] = value;
+		setEquations(newEquations);
+	};
 
 	return (
 		<div>
@@ -34,7 +48,7 @@ export default function menuIconButton(): React.ReactElement {
 				}}
 			>
 				<Box p={2} width='500px' textAlign='center' role='presentation'>
-					<Stack direction='row'>
+					<Stack direction='row' alignItems='center' justifyContent='space-between'>
 						<Typography variant='h6' component='div'>
 							Input Equation
 						</Typography>
@@ -46,6 +60,24 @@ export default function menuIconButton(): React.ReactElement {
 							<ArrowBackIosNewIcon />
 						</IconButton>
 					</Stack>
+					<Box mt={2}>
+						{equations.map((equation, index) => (
+							<Box key={index} mb={2}>
+								<TextField
+									label={`Equation ${index + 1}`}
+									variant='outlined'
+									fullWidth
+									value={equation}
+									onChange={(e) => {
+										handleChange(index, e.target.value);
+									}}
+								/>
+							</Box>
+						))}
+						<IconButton onClick={addField}>
+							<AddIcon />
+						</IconButton>
+					</Box>
 				</Box>
 			</Drawer>
 		</div>
