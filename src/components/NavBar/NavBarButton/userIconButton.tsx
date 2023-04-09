e447458354c2674572ff8bc005ec58f08de1b258
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-10 00:14:44 +0700

    Added ClickAwayListener to navbar user icon

--- src/components/NavBar/NavBarButton/userIconButton.tsx
+++ src/components/NavBar/NavBarButton/userIconButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { Menu, MenuItem } from '@mui/material';
+import { ClickAwayListener, Menu, MenuItem } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../../config/FirebaseConfig';
 import { signOut } from 'firebase/auth';
@@ -33,33 +33,37 @@ function userIconButton(): React.ReactElement {
 	};
 
 	return (
-		<IconButton
-			data-testid='user-icon-button'
-			size='large'
-			edge='start'
-			color='inherit'
-			aria-label='menu'
-			sx={{ mr: 2 }}
-			aria-controls={open ? 'basic-menu' : undefined}
-			aria-haspopup='true'
-			aria-expanded={open ? 'true' : undefined}
-			onClick={handleClick}
-		>
-			<AccountCircleIcon />
+		<ClickAwayListener onClickAway={handleClose}>
+			<div>
+				<IconButton
+					data-testid='user-icon-button'
+					size='large'
+					edge='start'
+					color='inherit'
+					aria-label='menu'
+					sx={{ mr: 2 }}
+					aria-controls={open ? 'basic-menu' : undefined}
+					aria-haspopup='true'
+					aria-expanded={open ? 'true' : undefined}
+					onClick={handleClick}
+				>
+					<AccountCircleIcon />
+				</IconButton>
 
-			<Menu
-				id='basic-menu'
-				anchorEl={anchorEl}
-				open={open}
-				onClose={handleClose}
-				MenuListProps={{
-					'aria-labelledby': 'basic-button',
-				}}
-			>
-				<MenuItem onClick={handleClickMyGraph}>My Graph</MenuItem>
-				<MenuItem onClick={handleClickLogOut}>Logout</MenuItem>
-			</Menu>
-		</IconButton>
+				<Menu
+					id='basic-menu'
+					anchorEl={anchorEl}
+					open={open}
+					onClose={handleClose}
+					MenuListProps={{
+						'aria-labelledby': 'basic-button',
+					}}
+				>
+					<MenuItem onClick={handleClickMyGraph}>My Graph</MenuItem>
+					<MenuItem onClick={handleClickLogOut}>Logout</MenuItem>
+				</Menu>
+			</div>
+		</ClickAwayListener>
 	);
 }
 
