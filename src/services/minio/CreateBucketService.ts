855bce1b6a6a92cef0f46fcb4c97b195eaf4412e
suhavni <suhavni@gmail.com>
2023-04-10 18:21:55 +0700

    add some documentation

--- src/services/minio/CreateBucketService.ts
+++ src/services/minio/CreateBucketService.ts
@@ -1,6 +1,12 @@
 import minioClient from './MinioClient';
 import { type S3 } from '@aws-sdk/client-s3';
 
+/*
+This function creates a bucket if it does not exist
+	@param bucketName: string
+	@param client: S3
+	@return Promise<boolean> whether the bucket has been created or not
+ */
 const CreateBucket: (bucketName: string, client?: S3) => Promise<boolean> = async (bucketName: string, client?: S3) => {
 	client = client ?? minioClient;
 	try {
