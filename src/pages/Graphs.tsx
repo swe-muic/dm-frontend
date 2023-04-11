b0ba3d6714341edb4aa17f6f62f1dd2e94d01347
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-11 22:33:58 +0700

    WIP

--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../config/FirebaseConfig';
 import Button from '@mui/material/Button';
 import Navbar from '../components/NavBar/NavBar';
 import ThumbnailGrid from '../components/ThumbnailGrid/ThumbnailGrid';
+import RetrieveObjectService from '../services/minio/RetrieveObjectService';
 
-const images = [
+interface Image {
+	image: string;
+	title: string;
+	linkUrl: string;
+}
+
+const images: Image[] = [
 	{
 		image: 'https://via.placeholder.com/500x500.png',
 		title: 'Image 1',
@@ -17,11 +24,42 @@ const images = [
 		title: 'Image 2',
 		linkUrl: '/',
 	},
+	{
+		image: 'https://via.placeholder.com/500x500.png',
+		title: 'Image 3',
+		linkUrl: '/',
+	},
 ];
+
+const getImagesWithUrls = async (images: Image[]): Promise<Image[]> => {
+	const urls = await Promise.all(
+		images.map(async (img) => {
+			const url = await RetrieveObjectService('test-bucket', 'stars');
+			return url;
+		}),
+	);
+
+	return images.map((img, idx) => ({
+		...img,
+		image: urls[idx].length === 0 ? 'https://via.placeholder.com/500x500.png' : urls[idx],
+	}));
+};
 
 const Graphs: React.FunctionComponent = () => {
 	const thisAuth = auth;
 	const navigate = useNavigate();
+
+	const [imagesWithUrls, setImagesWithUrls] = useState<Image[]>([]);
+
+	useEffect(() => {
+		getImagesWithUrls(images)
+			.then((imagesWithUrls) => {
+				setImagesWithUrls(imagesWithUrls);
+			})
+			.catch((error) => {
+				console.error('Error fetching images with URLs:', error);
+			});
+	}, []);
 
 	const headerContainerStyle = {
 		display: 'flex',
@@ -52,13 +90,12 @@ const Graphs: React.FunctionComponent = () => {
 							navigate('/');
 						})
 						.catch((error) => {
-							/* istanbul ignore next */
 							console.warn(error);
 						});
 				}}
 			></Button>
 			<div style={containerStyle}>
-				<ThumbnailGrid images={images} columns={4} />
+				<ThumbnailGrid images={imagesWithUrls} columns={4} />
 			</div>
 		</div>
 	);
