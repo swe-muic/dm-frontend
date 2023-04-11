e3ffb2d182ebe9e4050cae0c7c4b77eb541733ef
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-11 19:16:20 +0700

    Refactored my-graphs page to use ThumbnailGrid/ThumbnailCard

--- src/components/ThumbnailCard/ThumbnailCard.test.tsx
+++ src/components/ThumbnailCard/ThumbnailCard.test.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import ThumbnailCard from './ThumbnailCard';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('ThumbnailCard component', () => {
 	test('renders correctly with given props', () => {
@@ -9,9 +10,15 @@ describe('ThumbnailCard component', () => {
 			image: 'https://via.placeholder.com/300x200',
 			title: 'Example Title',
 			description: 'Example Description',
+			linkUrl:
+				'https://example.com/?gclid=CjwKCAjwitShBhA6EiwAq3RqAxmfJUdD82AaTq3VdoSi0b1y1rnCZQKtbmPQLumvXJXGULsV13IAZRoCFdEQAvD_BwE',
 		};
 
-		const { getByText, getByAltText } = render(<ThumbnailCard {...props} />);
+		const { getByText, getByAltText } = render(
+			<BrowserRouter>
+				<ThumbnailCard {...props} />
+			</BrowserRouter>,
+		);
 
 		expect(getByAltText(props.title)).toBeInTheDocument();
 		expect(getByText(props.title)).toBeInTheDocument();
