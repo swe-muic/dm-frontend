f42a38e89bc62432b6b6cbc3d2301a6883f73f51
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-11 19:16:20 +0700

    Refactored my-graphs page to use ThumbnailGrid/ThumbnailCard

--- src/components/ThumbnailGrid/ThumbnailGrid.tsx
+++ src/components/ThumbnailGrid/ThumbnailGrid.tsx
@@ -7,6 +7,7 @@ interface Props {
 	images: Array<{
 		image: string;
 		title: string;
+		linkUrl: string;
 	}>;
 	columns?: number;
 }
@@ -15,7 +16,7 @@ const ThumbnailGrid: React.FC<Props> = ({ images, columns = 4 }) => (
 	<Grid container spacing={2}>
 		{images.map((image, index) => (
 			<Grid item xs={12} sm={6} md={12 / columns} key={index}>
-				<ThumbnailCard image={image.image} title={image.title} />
+				<ThumbnailCard image={image.image} title={image.title} linkUrl={image.linkUrl} />
 			</Grid>
 		))}
 	</Grid>
--- src/pages/Graphs.tsx
+++ src/pages/Graphs.tsx
@@ -4,56 +4,18 @@ import { useNavigate } from 'react-router-dom';
 import { auth } from '../config/FirebaseConfig';
 import Button from '@mui/material/Button';
 import Navbar from '../components/NavBar/NavBar';
-import ImageGrid from '../components/ImageGrid/ImageGrid';
+import ThumbnailGrid from '../components/ThumbnailGrid/ThumbnailGrid';
 
 const images = [
 	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 1',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 1 Link',
+		image: 'https://via.placeholder.com/500x500.png',
+		title: 'Image 1',
+		linkUrl: '/',
 	},
 	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 2',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 2 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 3',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 3 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 4',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 4 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 5',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 5 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 6',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 6 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 7',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 7 Link',
-	},
-	{
-		src: 'https://via.placeholder.com/500x500.png',
-		alt: 'Image 8',
-		linkUrl: 'https://example.com',
-		linkText: 'Image 8 Link',
+		image: 'https://via.placeholder.com/500x500.png',
+		title: 'Image 2',
+		linkUrl: '/',
 	},
 ];
 
@@ -65,7 +27,7 @@ const Graphs: React.FunctionComponent = () => {
 		display: 'flex',
 		paddingLeft: '8%',
 		paddingTop: '1%',
-		paddingBottom: '2%',
+		paddingBottom: '1%',
 		fontFamily: 'Monospace',
 		fontSize: '40px',
 		fontWeight: 'Normal',
@@ -96,7 +58,7 @@ const Graphs: React.FunctionComponent = () => {
 				}}
 			></Button>
 			<div style={containerStyle}>
-				<ImageGrid images={images} columns={4} />
+				<ThumbnailGrid images={images} columns={4} />
 			</div>
 		</div>
 	);
