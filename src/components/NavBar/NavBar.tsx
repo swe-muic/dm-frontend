7c6e0b71b2daddfe55c03111d7a1c599b87f0096
Rinradee Sae-lao <rinradee.sae@gmail.com>
2023-04-12 02:00:28 +0700

    connected 'save graphs' to backend api

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -22,6 +22,32 @@ export interface NavbarProps {
 	forceLogin?: boolean;
 }
 
+interface Graph {
+	name: string;
+	preview: string;
+	owner: string;
+	created: string;
+	updated: string;
+}
+
+// interface GraphRequest {
+// 	name: string;
+// 	preview: 'minio_bucket_test';
+// 	owner: string;
+// }
+//
+interface GraphDetailResponse {
+	status: number;
+	message: string;
+	data: Graph;
+}
+
+interface GraphValidationErrorResponse {
+	status: number;
+	message: string;
+	data: Graph;
+}
+
 export default function Navbar(props: NavbarProps): React.ReactElement {
 	const { currentPage, forceLogin } = props;
 	const navigate = useNavigate();
@@ -51,8 +77,91 @@ export default function Navbar(props: NavbarProps): React.ReactElement {
 		setIsLogin(!isLogIn);
 	};
 
-	const handleSaveIconClick = (): void => {
+	const isGraphExist = async (graphId: number): Promise<boolean> => {
+		try {
+			const response = await fetch(`http://127.0.0.1:8000/api/viewset/graphs/${graphId}/`, {
+				method: 'GET',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+					// Add any other headers you need, such as authorization headers
+				},
+			});
+
+			if (response.status === 200) {
+				return true; // Graph exists
+			} else if (response.status === 404) {
+				return false; // Graph does not exist
+			}
+			console.log(response);
+		} catch (e) {
+			console.log(e);
+		}
+		return false;
+	};
+
+	const updateGraph = async (graphId: number): Promise<void> => {
+		const dateTimeNow = new Date();
+		const graphReq = {
+			name: buttonText,
+			preview: 'minio_bucket_test2',
+			updated: dateTimeNow.toDateString(),
+			owner: auth.currentUser?.uid,
+		};
+		const response = await fetch(`http://127.0.0.1:8000/api/viewset/graphs/${graphId}/`, {
+			method: 'PUT',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(graphReq),
+		});
+		if (!response.ok) {
+			const errorResponse: GraphValidationErrorResponse = await response.json();
+			throw new Error(errorResponse.message);
+		}
+		const graphDetail: GraphDetailResponse = await response.json();
+		console.log(graphDetail);
+	};
+
+	const createGraph = async (): Promise<void> => {
+		// const dateTimeNow = new Date();
+		const graphReq = {
+			name: buttonText,
+			preview: 'minio_bucket_test',
+			owner: auth.currentUser?.uid,
+		};
+		const response = await fetch('http://127.0.0.1:8000/api/viewset/graphs/', {
+			method: 'POST',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(graphReq),
+		});
+		if (!response.ok) {
+			const errorResponse: GraphValidationErrorResponse = await response.json();
+			throw new Error(errorResponse.message);
+		}
+		const graphDetail: GraphDetailResponse = await response.json();
+		console.log(graphDetail);
+	};
+
+	const handleSaveIconClick = async (): Promise<void> => {
+		const user = auth.currentUser;
 		setIsSave(true);
+		if (user != null) {
+			console.log(user);
+			const isExist = await isGraphExist(777);
+			console.log(isExist);
+			if (isExist) {
+				await updateGraph(777);
+			} else {
+				await createGraph();
+			}
+		} else {
+			console.log('null user');
+		}
 	};
 
 	const handleEditGraphName = (): void => {
@@ -85,6 +194,7 @@ export default function Navbar(props: NavbarProps): React.ReactElement {
 					</Stack>
 				) : null}
 
+				{/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
 				{currentPage === 'home' ? checkIsLogin(isLogIn, handleSaveIconClick, isSave, handleLoginRegisClick) : null}
 			</Toolbar>
 		</AppBar>
