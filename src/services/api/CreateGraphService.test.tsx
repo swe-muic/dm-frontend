9c709611c7af69bbfcfec63b0fadf27ec3acda83
suhavni <suhavni@gmail.com>
2023-04-13 03:02:25 +0700

    added tests uncovered tests for delete graph svc

--- src/services/api/CreateGraphService.test.tsx
+++ src/services/api/CreateGraphService.test.tsx
@@ -3,11 +3,13 @@
 import CreateGraph from './CreateGraphService';
 import DeleteGraph from './DeleteGraphService';
 
-global.fetch = jest.fn().mockResolvedValue({
-	json: jest.fn().mockResolvedValue(1),
-});
-
 describe('Create a graph', () => {
+	beforeEach(() => {
+		global.fetch = jest.fn().mockResolvedValue({
+			json: jest.fn().mockResolvedValue(1),
+		});
+	});
+
 	it('should return a Promise', async () => {
 		try {
 			const result = CreateGraph('test graph title', '1');
@@ -19,12 +21,23 @@ describe('Create a graph', () => {
 		}
 	});
 
-	// it('should return graph id', async () => {
-	// 	const result = await CreateGraph('test graph title', '1');
-	// 	expect(result).toEqual();
-	//
-	// 	await DeleteGraph(result);
-	// });
+	it('should return graph id', async () => {
+		const passedResult = {
+			ok: true,
+			json: jest.fn().mockResolvedValue({
+				data: {
+					id: 2,
+				},
+			}),
+		};
+
+		global.fetch = jest.fn().mockResolvedValue(passedResult);
+
+		const result = await CreateGraph('test graph title', '2');
+		expect(result).toEqual(2);
+
+		await DeleteGraph(result);
+	});
 
 	it('should return undefined graph id, already exist', async () => {
 		await CreateGraph('test graph title', '1');
