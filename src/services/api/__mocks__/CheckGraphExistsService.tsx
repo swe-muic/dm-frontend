f29afe93cfff6e9c32db7fefb4f4ed6b0cc27f7d
Rinradee S <56635619+qwertybee@users.noreply.github.com>
2023-04-12 23:29:39 +0700

    clean up

--- src/services/api/__mocks__/CheckGraphExistsService.tsx
+++ src/services/api/__mocks__/CheckGraphExistsService.tsx
@@ -1,7 +1,5 @@
 /* eslint-env jest */
 import type GraphInterface from '../../../interfaces/schema/GraphObjectInterface';
-// import type GraphDetailResponse from '../../../interfaces/schema/GraphDetailResponseInterface';
-// import type NotFoundErrorResponse from '../../../interfaces/schema/GraphNotFoundErrorResponseInterface';
 
 const mockedGraph: GraphInterface = {
 	id: 1,
@@ -11,18 +9,6 @@ const mockedGraph: GraphInterface = {
 	created: '2023-04-12T02:07:26.256Z',
 	updated: '2023-04-12T02:07:26.256Z',
 };
-
-// const mockedGraphDetailResponse: GraphDetailResponse = {
-// 	status: 200,
-// 	message: 'OK',
-// 	data: mockedGraph,
-// };
-//
-// const mockedGraphNotFoundErrorResponse: NotFoundErrorResponse = {
-// 	status: 404,
-// 	message: 'NOT FOUND',
-// 	data: 'NOT FOUND',
-// };
 
 const GraphExists = jest.fn().mockImplementation(async (graphId: number) => graphId === mockedGraph.id);
 
