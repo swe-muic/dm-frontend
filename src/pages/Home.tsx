6bf9f2124e7ea21c3e5c2d54d3b591fd8d1e0143
suhavni <suhavni@gmail.com>
2023-04-13 01:23:05 +0700

    extract utils to separate folder

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,9 +7,7 @@ import LineStyleEnum from '../enum/LineStyleEnum';
 import RetrieveParsedEquationsService from '../services/api/RetrieveParsedEquationsService';
 import { isErrorResponseInterface } from '../interfaces/response/ErrorResponseInterface';
 import { useSearchParams } from 'react-router-dom';
-
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { parse } from 'mathjs';
+import HomePageUtils from '../utils/HomePageUtils';
 
 const Home: React.FunctionComponent = () => {
 	const [equations, setEquations] = useState<FunctionInterface[]>([]);
@@ -28,31 +26,6 @@ const Home: React.FunctionComponent = () => {
 		index: equations.length,
 	};
 
-	function splitExpression(expression: string): string {
-		const splittedExpression = expression.split('=');
-		return splittedExpression[splittedExpression.length - 1].replace(/\s/g, '');
-	}
-
-	function isPlottableHelper(fn: string, x: number): boolean {
-		try {
-			// Try to evaluate the function with x = 0
-			// eslint-disable-next-line no-eval
-			const f = parse(fn).compile();
-
-			// Test the function at x=0
-			const evaluatedF = f.evaluate({ x });
-			return typeof evaluatedF === 'number' && !Number.isNaN(evaluatedF);
-		} catch (error) {
-			console.log(error);
-			return false;
-		}
-	}
-
-	function isPlottable(fn: string): boolean {
-		const xValues = [-100, -10, -1, 0, 1, 10, 100];
-		return xValues.some((x) => isPlottableHelper(fn, x));
-	}
-
 	const handlePlotData: () => Promise<void> = async () => {
 		const equationsString = equations.map((equation) => equation.equation);
 		const parsedEquationResponse = await RetrieveParsedEquationsService(equationsString);
@@ -65,11 +38,11 @@ const Home: React.FunctionComponent = () => {
 				.filter((equations) => equations.equation.length > 0)
 				.map((equation) => ({
 					...equation,
-					stringEquation: splitExpression(parsedEquationResponse.data.parsed_expressions[equation.index]),
+					stringEquation: HomePageUtils.splitExpression(parsedEquationResponse.data.parsed_expressions[equation.index]),
 				}))
-				.filter((equation) => isPlottable(equation.stringEquation))
+				.filter((equation) => HomePageUtils.isPlottable(equation.stringEquation))
 				.map((equation) => ({
-					fn: splitExpression(parsedEquationResponse.data.parsed_expressions[equation.index]),
+					fn: HomePageUtils.splitExpression(parsedEquationResponse.data.parsed_expressions[equation.index]),
 					color: equation.color,
 					graphType: equation.lineStyle,
 					nSamples: equation.lineStyle === LineStyleEnum.DOTTED ? 150 : undefined,
---
+++ src/utils/HomePageUtils.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { parse } from 'mathjs';
+
+function splitExpression(expression: string): string {
+	const splittedExpression = expression.split('=');
+	return splittedExpression[splittedExpression.length - 1].replace(/\s/g, '');
+}
+
+function isPlottableHelper(fn: string, x: number): boolean {
+	try {
+		// Try to evaluate the function with x = 0
+		// eslint-disable-next-line no-eval
+		const f = parse(fn).compile();
+
+		// Test the function at x=0
+		const evaluatedF = f.evaluate({ x });
+		return typeof evaluatedF === 'number' && !Number.isNaN(evaluatedF);
+	} catch (error) {
+		console.log(error);
+		return false;
+	}
+}
+
+function isPlottable(fn: string): boolean {
+	const xValues = [-100, -10, -1, 0, 1, 10, 100];
+	return xValues.some((x) => isPlottableHelper(fn, x));
+}
+
+export default { splitExpression, isPlottable };
