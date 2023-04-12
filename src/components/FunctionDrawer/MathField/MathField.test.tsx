49d4e1760a85f9e9dd8306bdcb4a584cdc42e779
DrNattapoom <nattapoom.dr@gmail.com>
2023-04-12 15:14:54 +0700

    add test for MathField

--- src/components/FunctionDrawer/MathField/MathField.test.tsx
+++ src/components/FunctionDrawer/MathField/MathField.test.tsx
@@ -1,34 +1,39 @@
 /* eslint-env jest */
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 import MathField, { type MathFieldProps } from './MathField';
 
 describe('Test MathField', () => {
-	const mockSetEquation = jest.fn();
-	jest.mock('react', () => ({
-		...jest.requireActual('react'),
-		useState: (initialValue: string) => [initialValue, mockSetEquation],
-	}));
-
-	let props: MathFieldProps;
-
-	beforeAll(() => {
-		const equation = 'x^2';
-		props = {
+	it('should update the equation when the input changes', () => {
+		let equation = 'x^2';
+		const handleChange = jest.fn((index: number, value: string) => {
+			equation = value;
+		});
+		const props: MathFieldProps = {
 			equation,
 			index: 0,
-			handleChange: mockSetEquation,
+			handleChange,
 		};
-	});
-
-	it('should update the equation when the input changes', () => {
 		const { getByLabelText } = render(<MathField {...props} />);
 		const mathField = getByLabelText(`Equation ${props.index + 1}`);
+		const input = mathField.querySelector('span > textarea') as HTMLTextAreaElement;
 
-		fireEvent.input(mathField, {
-			target: {
-				latex: '2x+3',
-			},
-		});
+		fireEvent.change(input, { target: { value: '2x+3' } });
+	});
+
+	let equation = '2x + 3 = 7';
+	const handleChange = jest.fn((index: number, value: string) => {
+		equation = value;
+	});
+	const props: MathFieldProps = {
+		equation,
+		index: 0,
+		handleChange,
+	};
+
+	it('renders an EditableMathField with the given equation', () => {
+		render(<MathField {...props} />);
+		const mathField = screen.getByRole('textbox');
+		expect(mathField).toBeInTheDocument();
 	});
 });
