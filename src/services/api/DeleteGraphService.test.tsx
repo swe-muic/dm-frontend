72a903603e49e3903dbbb2f2b2a66e8bf3f00e28
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-04-13 02:52:09 +0700

    Added DeleteGraphService test

--- src/services/api/DeleteGraphService.test.tsx
+++ src/services/api/DeleteGraphService.test.tsx
@@ -2,19 +2,26 @@
 
 import DeleteGraph from './DeleteGraphService';
 
-global.fetch = jest.fn().mockResolvedValue(undefined);
-
 describe('Delete a graph', () => {
 	it('should return a Promise', () => {
 		const result = DeleteGraph(1);
 		expect(result).toBeInstanceOf(Promise);
 	});
 
-	it('should return not found', async () => {
+	it('should throw an error if the response is not OK', async () => {
+		global.fetch = jest.fn().mockResolvedValue({ ok: false });
+
 		try {
-			await DeleteGraph(-1);
+			await DeleteGraph(1);
 		} catch (e) {
-			expect(e).toEqual('NOT FOUND');
+			expect(e).toEqual(new Error('NOT FOUND'));
+			expect.assertions(1);
 		}
+	});
+
+	it('should be OK', async () => {
+		global.fetch = jest.fn().mockResolvedValue({ ok: true });
+		await DeleteGraph(1);
+		expect.assertions(0);
 	});
 });
